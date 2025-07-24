dd3181b7d6a82750bb93c3652bef1511f575629b
Roshal <roshalsebastian10@gmail.com>
2025-07-24 18:56:58 -0500

    added image, text field button, and text

--- NavigationStack/SecondView.swift
+++ NavigationStack/SecondView.swift
@@ -17,11 +17,11 @@ struct SecondView: View {
                 NavigationLink(destination: ThirdView()) {
                     Text("for a surprise!")
                         .bold()
-                        .foregroundStyle(.blue)
+                        .foregroundStyle(.black)
                         .padding(20.0)
                         .background(
                             RoundedRectangle(cornerRadius: 45)
-                               .fill(Color(red: 0.9, green: 0.8, blue: 1.0))
+                               .fill(Color(red: 0.1, green: 0.8, blue: 1.0))
                                 .frame(width: 200, height: 60)
                         )
                 }
--- NavigationStack/ThirdView.swift
+++ NavigationStack/ThirdView.swift
@@ -6,33 +6,48 @@
 //
 import SwiftUI
 
+
 struct ThirdView: View {
+    @State private var name: String = ""
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Oooh, go to the next page!")
+                Image("Image")
+                    .resizable()
+                    .frame(width: 250, height: 200)
+                Text("Guess who this artist is!")
                     .font(.title)
                     .padding(20)
-                NavigationLink(destination: FourthView()) {
-                    Text("for a surprise!")
-                        .bold()
-                        .foregroundStyle(.blue)
-                        .padding(20.0)
-                        .background(
-                            RoundedRectangle(cornerRadius: 45)
-                               .fill(Color(red: 0.9, green: 0.8, blue: 1.0))
-                                .frame(width: 200, height: 60)
-                        )
+                
+                TextField("Type here...", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.horizontal)
+                if name.lowercased() == "ariana grande" {
+                    NavigationLink("Go to next page", destination: FourthView())
+                    
                 }
+                
+                    NavigationLink(destination: FourthView()) {
+                        Text("Let's see if you are right")
+                            .padding(20.0)
+                            .bold()
+                            .foregroundStyle(.blue)
+                            .padding(20.0)
+                            .background(
+                                RoundedRectangle(cornerRadius: 45)
+                                    .fill(Color(red: 0.9, green: 0.8, blue: 1.0))
+                                    .frame(width: 250, height: 60)
+                            )
+                    }
+                }
+                
             }
-            
+            .navigationTitle("ThirdPage")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
         }
-        .navigationTitle("SecondPage")
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarHidden(true)
     }
-    
-}
     #Preview {
         ThirdView()
     }
+
