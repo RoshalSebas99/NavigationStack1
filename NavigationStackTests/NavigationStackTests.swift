4b356865e3455870fa809fd07192d62f0fb0d938
Roshal <roshalsebastian10@gmail.com>
2025-07-24 12:19:53 -0500

    Initial Commit

---
+++ NavigationStackTests/NavigationStackTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackTests.swift
+//  NavigationStackTests
+//
+//  Created by Roshal Sebastian on 7/24/25.
+//
+
+import Testing
+@testable import NavigationStack
+
+struct NavigationStackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
